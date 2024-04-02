04e45882dccbea3057d64ddd44eacbdaa5a97b52
Frank <frankdewijk@gmail.com>
2024-04-02 13:27:08 +0200

    add gpt4 limits

--- src/limits.ts
+++ src/limits.ts
@@ -15,6 +15,9 @@ export class TokenLimits {
     } else if (model === 'gpt-4') {
       this.maxTokens = 8000
       this.responseTokens = 2000
+    } else if (model === "gpt-4-turbo-preview") {
+      this.maxTokens = 128000
+      this.responseTokens = 4000
     } else {
       this.maxTokens = 4000
       this.responseTokens = 1000
